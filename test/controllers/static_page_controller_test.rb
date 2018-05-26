d56a14fb2d170c55bec9f6abc043f3ad86cca5bb
Tim Fleming <tfleming26@hotmail.com>
2018-05-26 15:43:03 -0600

    Add base title assertion

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_page_home_url
-    assert_response :success
-  end
 
+  def setup
+    @base_title = "Task Planify"
+  end
+  
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
 end
