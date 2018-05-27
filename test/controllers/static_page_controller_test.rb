839eb43e1f8a5ac4d9badaedcf9d4685a547e2eb
Tim Fleming <tfleming26@hotmail.com>
2018-05-26 22:24:35 -0600

    Update assertion in home controller test

--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get home" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
