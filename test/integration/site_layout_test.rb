ee17e5d69f7679032bb54f1175885dd6a8c01771
Tim Fleming <tfleming26@hotmail.com>
2018-05-27 02:11:46 -0600

    Add title assertion for signup page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template "static_page/home"
-    assert_select "a[href=?]", root_path, count: 2      
+    assert_select "a[href=?]", root_path, count: 2 
+    get signup_path    
+    assert_select "title", full_title("Sign Up")     
   end
 end
