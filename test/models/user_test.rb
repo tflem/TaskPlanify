f6bd50c7d1d56a18154bd4e3a1277f19834603d8
Tim Fleming <tfleming26@hotmail.com>
2018-05-29 08:52:42 -0600

    Add test for initial validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(name: "Patrick Star", email: "patrick@bb.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
 end
