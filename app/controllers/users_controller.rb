f95099560a691644fbb6d05a07c6f513f65637e5
Tim Fleming <tfleming26@hotmail.com>
2018-06-08 11:30:58 -0600

    Add create action with strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(params[user_params])
+    if @user.save
+      # render successful flash message
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.friendly.find(params[:id])
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
