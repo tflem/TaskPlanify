176cc0bca644795f8c137fd6d02d1fddeb3d2a5a
Tim Fleming <tfleming26@hotmail.com>
2018-06-09 13:41:58 -0600

    Add flash message to user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,19 +3,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(params[user_params])
-    if @user.save
-      redirect_to @user
-      # render successful flash message
-    else
-      render 'new'
-    end
-  end
-
   def show
     @user = User.friendly.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save      
+      flash[:success] = "Welcome to Task Planify!"
+      redirect_to @user
+    else
+      render "new"
+    end
+  end  
 
   private
 
