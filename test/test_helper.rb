0e6ece84d7745d2c36af3e54051de37144f5461f
Tim Fleming <tfleming26@hotmail.com>
2018-06-19 10:59:41 -0600

    Add is signed in? method inside tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,5 +17,7 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
+  def is_signed_in?
+    !session[:user_id].nil?
+  end
 end
