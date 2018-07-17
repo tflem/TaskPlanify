25182eb3be438251ffafbb5a8c32fad49091a57a
Tim Fleming <tfleming26@hotmail.com>
2018-07-17 10:14:49 -0600

    Add new token and remember methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   extend FriendlyId
   friendly_id :name, use: :slugged
   validates :name, presence: true, length: { maximum: 50 }
@@ -15,5 +16,15 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns random token for remember digest
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
